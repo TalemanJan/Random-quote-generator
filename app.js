36141d9c484e69b0c6be606593f2caf40e72c5e2
talemanjan <talemanaptech@gmail.com>
2023-03-18 15:40:23 +0500

    added js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let quotes=[
 
 "“Be yourself; everyone else is already taken ― Oscar Wilde.”",
+"“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.” ―Albert Einstein",
 
 
 ]

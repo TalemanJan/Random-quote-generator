f4d4e6fa6a41492a2135f20ec47231806e71123e
talemanjan <talemanaptech@gmail.com>
2023-03-18 15:40:34 +0500

    added js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let quotes=[
 
 "“Be yourself; everyone else is already taken ― Oscar Wilde.”",
 "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.” ―Albert Einstein",
+" “I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”― Marilyn Monroe",
 
 
 ]

20382b06069f9bb4f2331b28a21638f53defead3
talemanjan <talemanaptech@gmail.com>
2023-03-18 15:42:05 +0500

    added js

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ let quotes=[
 "“You've gotta dance like there's nobody watching,Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth.”― William W. Purkey",
 "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”― Dr. Seuss",
 "“You only live once, but if you do it right, once is enough.”― Mae West",
-
+"“Be the change that you wish to see in the world.”― Mahatma Gandhi",
+"“In three words I can sum up everything I've learned about life: it goes on.”― Robert Frost",
 
 ]
 

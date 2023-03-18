5cc6144182e8df9851a9cd6ac2f2ea2a7fde0426
talemanjan <talemanaptech@gmail.com>
2023-03-18 15:41:08 +0500

    added js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let quotes=[
 " “I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”― Marilyn Monroe",
 "“So many books, so little time.”― Frank Zappa",
 "“A room without books is like a body without a soul.”― Marcus Tullius Cicero",
+"“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”― Bernard M. Baruch",
 
 
 ]

fc2a9bb0ba632a66c654c9bbf126d47bf78559a5
talemanjan <talemanaptech@gmail.com>
2023-03-18 15:41:49 +0500

    added js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let quotes=[
 "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”― Bernard M. Baruch",
 "“You've gotta dance like there's nobody watching,Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth.”― William W. Purkey",
 "“You know you're in love when you can't fall asleep because reality is finally better than your dreams.”― Dr. Seuss",
+"“You only live once, but if you do it right, once is enough.”― Mae West",
 
 
 ]

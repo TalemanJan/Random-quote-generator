03c864a1413dc1297bdc430e47fbb63d70d2504e
talemanjan <talemanaptech@gmail.com>
2023-03-18 15:40:56 +0500

    added js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let quotes=[
 "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.” ―Albert Einstein",
 " “I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”― Marilyn Monroe",
 "“So many books, so little time.”― Frank Zappa",
+"“A room without books is like a body without a soul.”― Marcus Tullius Cicero",
 
 
 ]

536520fb23847180118ece73dba1d766b2f51c7c
talemanjan <talemanaptech@gmail.com>
2023-03-18 15:41:18 +0500

    added js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let quotes=[
 "“So many books, so little time.”― Frank Zappa",
 "“A room without books is like a body without a soul.”― Marcus Tullius Cicero",
 "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”― Bernard M. Baruch",
+"“You've gotta dance like there's nobody watching,Love like you'll never be hurt,Sing like there's nobody listening,And live like it's heaven on earth.”― William W. Purkey",
 
 
 ]
